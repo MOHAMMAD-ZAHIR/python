3bc7bbc77fb3968a6ecc9cd5c207c2ffc2e8e6b6
MD ZAWHARUL ISLAM <57909842+MOHAMMAD-ZAHIR@users.noreply.github.com>
2020-06-13 14:13:02 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+print("Fibinacci number printing : ")
+value = int(input("Enter number : "))
+
+
+def fib(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
+fib(value)
